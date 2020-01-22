3d7273bede2dc2f33f59832b8eb51ada604b37ae
AndreiMitriakov <andrei.mitriakov@gmail.com>
2020-01-22 01:08:20 +0100

    dijk

---
+++ codewars/3kyu/dijkstra_alpinist/cmake-build-debug/CMakeFiles/dijkstra_alpinist.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dijkstra_alpinist.dir/main.cpp.o"
+  "dijkstra_alpinist"
+  "dijkstra_alpinist.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dijkstra_alpinist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- codewars/3kyu/dijkstra_alpinist/main.cpp
+++ codewars/3kyu/dijkstra_alpinist/main.cpp
@@ -1,104 +1,108 @@
-#include <iostream>
+#include <queue>
 #include <vector>
+#include <iostream>
 #include <limits>
+#include <unordered_map>
+#include <sstream>
+
 using namespace std;
+/*
+    Dijkstra algorithm implementation
+*/
 
-int V;
+int INFINITY = std::numeric_limits<int>::max();
 
-struct PriorityQueue {
-  public:
-	vector<vector<Node>> queue;
-	void add_node(Node node) {
-	  bool not_inserted = true;
-	  for (int i=queue.begin(); i<queue.end(); ++i) {
-	    if (queue[i].value > node.value) {
-	      queue.insert(queue.begin()+i, node);	    
-	      inserted = false;
-	      break;
-	    }  
-	  }
-	  if (not_inserted)
-	    queue.push_back(node);
-	}
-	Node pop() {
-	  queue.pop_front();
-	}
+class Node {};
+
+class Graph {
+public:
+    Node get_current()
+    {}
+};
+
+// https://stackoverflow.com/questions/9178083/priority-queue-for-user-defined-types
+
+struct Comp{
+    bool operator()(const Node& a, const Node& b){
+        return a.value<b.value;
+    }
+};
+vector<pair<int, int>> find_dijkstra_path(pair<int, int> start, pair<int, int> end, Graph graph)
+{
+    pair<int, int> point;
+    vector<pair<int, int>> path;
+    Node cur;
+    priority_queue< Node, vector<Node>, Comp> q;
+    vector<Node> neighbors;
+    q.push(graph.get_start())
+    while (true)
+    {
+        cur = q.pop();
+        cur.visited = true;
+        neighbors = cur.get_neighbors();
+        for (auto n: neighbors)
+        {
+            n.value = graph.cost_map(n.first, n.second) + cur.value;
+            q.push(n);
+        }
+        if (graph.end.visited == true) {
+            break;
+        }
+    }
+    // create path back
+    // return path, cost;
 }
 
-struct Node {
-  public:
-	  int value = numeric_limits<int>::max();
-	  int x;
-	  int y;
-	  bool visited = false;
-	  vector<vector<Node>> neighbors;
-	  Node (int x_; int y_) {
-	  	x = x_;
-		y = y_;
-		if ((x-1>=0)&&(y-1>=0))
-		  neighbors.push_back(Node(x-1, y-1));
-		if ((x<V)&&(y-1>=0))
-		  neighbors.push_back(Node(x+1, y-1));
-		if ((x-1>=0)&&(y<V))
-		  neighbors.push_back(Node(x-1, y+1));
-		if ((x<V)&&(y<V))
-		  neighbors.push_back(Node(x+1, y+1));
-	  }
-};
-
-struct Graph {
-  public:
-  	    vector<vector<Node>> nodes;
-	    Node (vector<vector<int>> maze) {
-            for (int i=0; i < V; ++i) {
-                for (int j=0; j < V; ++j) {
-                
-                }
-            }
-	    }
-};
-
-
-int search (vector<vector<int>> maze)  {
-  pq = PriorityQueue();
-  pq.add_node(Node(0, 0));
-  while true {
-    
-    
-  }
-  return 0;
+template<typename T> void print_queue(T& q) {
+    while(!q.empty()) {
+        std::cout << q.top() << " ";
+        q.pop();
+    }
+    std::cout << '\n';
 }
 
 vector<vector<int>> get_maze(string maze) {
-  vector<vector<int>> v; 
-  vector<int> d;
-  for (auto x: maze) {
-    if ((x>=48) && (x<=57)) {
-        d.push_back((int)x-48);
-    }
-    else {
-        if (d.size() != 0) {
-            v.push_back(d);
-            d.clear();
+    vector<vector<int>> v;
+    vector<int> d;
+    for (auto x: maze) {
+        if ((x>=48) && (x<=57)) {
+            d.push_back((int)x-48);
         }
-        
+        else {
+            if (d.size() != 0) {
+                v.push_back(d);
+                d.clear();
+            }
+
+        }
     }
-  }
-  v.push_back(d);
-  return v;
+    v.push_back(d);
+    return v;
 }
 
-int main () {
-  std::string s5 = 
-  "700000\n"
-  "077770\n"
-  "077770\n"
-  "077770\n"
-  "077770\n"
-  "000007";
+void print(vector<vector<int>> v)
+{
+    for (auto d: v) {
+        for (auto c: d) {
+            cout << c;
+        }
+        cout << endl;
+    }
+}
 
-  vector<vector<int>> maze = get_maze(s5);
-  V = maze.size(); 
-  cout << search(maze) << end;
-  return 0;
+int main() {
+    string maze = "000\n190\n000";
+    vector<vector<int>> maze_;
+    maze_ = get_maze(maze);
+    Graph graph(maze_);
+    pair<int, int> start(0, 0);
+    pair<int, int> end(maze_.size(), maze_.size());
+    vector<pair<int, int>> path = find_dijkstra_path(start, end, graph);
+    for (auto p: path)
+    {
+        cout << p.first << " " << p.second;
+    }
+    std::priority_queue<int> q;
+    std::priority_queue<int, std::vector<int>, std::greater<int> > q2;
+    return 0;
 }
