cd149ce15cc63dc4c2f1bc8bb00f86a8464b0e64
AndreiMitriakov <andrei.mitriakov@gmail.com>
2020-05-20 03:52:38 +0200

    merge

---
+++ sorting_algorithms/merge_sort/merge_sort.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <chrono> 
+using namespace std::chrono; 
+using namespace std;
+
+void merge(vector<int>& arr, int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    /* create temp arrays */
+    int L[n1], R[n2]; 
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
+/* l is for left index and r is right index of the 
+   sub-array of arr to be sorted */
+void mergeSort(vector<int>& arr, int l, int r) 
+{ 
+    if (l < r) 
+    { 
+        // Same as (l+r)/2, but avoids overflow for 
+        // large l and h 
+        int m = l+(r-l)/2; 
+  
+        // Sort first and second halves 
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m+1, r); 
+  
+        merge(arr, l, m, r); 
+    } 
+} 
+
+int main() {
+    auto start = high_resolution_clock::now();
+    vector<int> v = {1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,
+1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,
+1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,
+1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,
+1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7,1,2,54,8,3,4,-1,5,0,7};
+    mergeSort(v, 0, v.size()-1);
+    auto stop = high_resolution_clock::now();
+    for (auto el: v)
+        cout << el << " ";
+    cout << endl;
+    auto duration = duration_cast<microseconds>(stop - start); 
+    cout << duration.count() << endl; 
+    return 0;
+}
